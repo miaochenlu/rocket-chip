96049ec88f06e1ee2e7afb4e7ce6c7075c30a162
miaochenlu <chenlu.miao@intel.com>
2024-08-16 10:45:55 +0800

    coherence use ParallelLookUp

--- src/main/scala/tilelink/Metadata.scala
+++ src/main/scala/tilelink/Metadata.scala
@@ -16,7 +16,7 @@ object ClientStates {
   def Trunk   = 2.U(width.W)
   def Dirty   = 3.U(width.W)
 
-  def hasReadPermission(state: UInt): Bool = state > Nothing
+  def hasReadPermission(state:  UInt): Bool = state > Nothing
   def hasWritePermission(state: UInt): Bool = state > Branch
 }
 
@@ -27,70 +27,88 @@ object MemoryOpCategories extends MemoryOpConstants {
 
   def categorize(cmd: UInt): UInt = {
     val cat = Cat(isWrite(cmd), isWriteIntent(cmd))
-    //assert(cat.isOneOf(wr,wi,rd), "Could not categorize command.")
+    // assert(cat.isOneOf(wr,wi,rd), "Could not categorize command.")
     cat
   }
 }
 
-/** Stores the client-side coherence information,
-  * such as permissions on the data and whether the data is dirty.
-  * Its API can be used to make TileLink messages in response to
-  * memory operations, cache control oeprations, or Probe messages.
-  */
+/**
+ * Stores the client-side coherence information, such as permissions on the data
+ * and whether the data is dirty. Its API can be used to make TileLink messages
+ * in response to memory operations, cache control oeprations, or Probe
+ * messages.
+ */
 class ClientMetadata extends Bundle {
+
   /** Actual state information stored in this bundle */
   val state = UInt(ClientStates.width.W)
 
   /** Metadata equality */
-  def ===(rhs: UInt): Bool = state === rhs
+  def ===(rhs: UInt):           Bool = state === rhs
   def ===(rhs: ClientMetadata): Bool = state === rhs.state
   def =/=(rhs: ClientMetadata): Bool = !this.===(rhs)
 
   /** Is the block's data present in this cache */
   def isValid(dummy: Int = 0): Bool = state > ClientStates.Nothing
 
-  /** Determine whether this cmd misses, and the new state (on hit) or param to be sent (on miss) */
+  /**
+   * Determine whether this cmd misses, and the new state (on hit) or param to
+   * be sent (on miss)
+   */
   private def growStarter(cmd: UInt): (Bool, UInt) = {
     import MemoryOpCategories._
     import TLPermissions._
     import ClientStates._
     val c = categorize(cmd)
-    MuxTLookup(Cat(c, state), (false.B, 0.U), Seq(
-    //(effect, am now) -> (was a hit,   next)
-      Cat(rd, Dirty)   -> (true.B,  Dirty),
-      Cat(rd, Trunk)   -> (true.B,  Trunk),
-      Cat(rd, Branch)  -> (true.B,  Branch),
-      Cat(wi, Dirty)   -> (true.B,  Dirty),
-      Cat(wi, Trunk)   -> (true.B,  Trunk),
-      Cat(wr, Dirty)   -> (true.B,  Dirty),
-      Cat(wr, Trunk)   -> (true.B,  Dirty),
-    //(effect, am now) -> (was a miss,  param)
-      Cat(rd, Nothing) -> (false.B, NtoB),
-      Cat(wi, Branch)  -> (false.B, BtoT),
-      Cat(wi, Nothing) -> (false.B, NtoT),
-      Cat(wr, Branch)  -> (false.B, BtoT),
-      Cat(wr, Nothing) -> (false.B, NtoT)))
+    val result =
+      ParallelLookUp(
+        Cat(c, state),
+        Seq(
+          // (effect, am now) -> (was a hit,   next)
+          Cat(rd, Dirty)  -> Cat(true.B, Dirty),
+          Cat(rd, Trunk)  -> Cat(true.B, Trunk),
+          Cat(rd, Branch) -> Cat(true.B, Branch),
+          Cat(wi, Dirty)  -> Cat(true.B, Dirty),
+          Cat(wi, Trunk)  -> Cat(true.B, Trunk),
+          Cat(wr, Dirty)  -> Cat(true.B, Dirty),
+          Cat(wr, Trunk)  -> Cat(true.B, Dirty),
+          // (effect, am now) -> (was a miss,  param)
+          Cat(rd, Nothing) -> Cat(false.B, NtoB),
+          Cat(wi, Branch)  -> Cat(false.B, BtoT),
+          Cat(wi, Nothing) -> Cat(false.B, NtoT),
+          Cat(wr, Branch)  -> Cat(false.B, BtoT),
+          Cat(wr, Nothing) -> Cat(false.B, NtoT),
+        ),
+      )
+    (result(width), result(width - 1, 0))
   }
 
-  /** Determine what state to go to after miss based on Grant param
-    * For now, doesn't depend on state (which may have been Probed).
-    */
+  /**
+   * Determine what state to go to after miss based on Grant param For now,
+   * doesn't depend on state (which may have been Probed).
+   */
   private def growFinisher(cmd: UInt, param: UInt): UInt = {
     import MemoryOpCategories._
     import TLPermissions._
     import ClientStates._
     val c = categorize(cmd)
-    //assert(c === rd || param === toT, "Client was expecting trunk permissions.")
-    MuxLookup(Cat(c, param), Nothing)(Seq(
-    //(effect param) -> (next)
-      Cat(rd, toB)   -> Branch,
-      Cat(rd, toT)   -> Trunk,
-      Cat(wi, toT)   -> Trunk,
-      Cat(wr, toT)   -> Dirty))
+    // assert(c === rd || param === toT, "Client was expecting trunk permissions.")
+    ParallelLookUp(
+      Cat(c, param),
+      Seq(
+        // (effect param) -> (next)
+        Cat(rd, toB) -> Branch,
+        Cat(rd, toT) -> Trunk,
+        Cat(wi, toT) -> Trunk,
+        Cat(wr, toT) -> Dirty,
+      ),
+    )
   }
 
-  /** Does this cache have permissions on this block sufficient to perform op,
-    * and what to do next (Acquire message param or updated metadata). */
+  /**
+   * Does this cache have permissions on this block sufficient to perform op,
+   * and what to do next (Acquire message param or updated metadata).
+   */
   def onAccess(cmd: UInt): (Bool, UInt, ClientMetadata) = {
     val r = growStarter(cmd)
     (r._1, r._2, ClientMetadata(r._2))
@@ -99,14 +117,14 @@ class ClientMetadata extends Bundle {
   /** Does a secondary miss on the block require another Acquire message */
   def onSecondaryAccess(first_cmd: UInt, second_cmd: UInt): (Bool, Bool, UInt, ClientMetadata, UInt) = {
     import MemoryOpCategories._
-    val r1 = growStarter(first_cmd)
-    val r2 = growStarter(second_cmd)
-    val needs_second_acq = isWriteIntent(second_cmd) && !isWriteIntent(first_cmd)
-    val hit_again = r1._1 && r2._1
-    val dirties = categorize(second_cmd) === wr
+    val r1                 = growStarter(first_cmd)
+    val r2                 = growStarter(second_cmd)
+    val needs_second_acq   = isWriteIntent(second_cmd) && !isWriteIntent(first_cmd)
+    val hit_again          = r1._1 && r2._1
+    val dirties            = categorize(second_cmd) === wr
     val biggest_grow_param = Mux(dirties, r2._2, r1._2)
-    val dirtiest_state = ClientMetadata(biggest_grow_param)
-    val dirtiest_cmd = Mux(dirties, second_cmd, first_cmd)
+    val dirtiest_state     = ClientMetadata(biggest_grow_param)
+    val dirtiest_cmd       = Mux(dirties, second_cmd, first_cmd)
     (needs_second_acq, hit_again, biggest_grow_param, dirtiest_state, dirtiest_cmd)
   }
 
@@ -117,30 +135,39 @@ class ClientMetadata extends Bundle {
   private def shrinkHelper(param: UInt): (Bool, UInt, UInt) = {
     import ClientStates._
     import TLPermissions._
-    MuxTLookup(Cat(param, state), (false.B, 0.U, 0.U), Seq(
-    //(wanted, am now)  -> (hasDirtyData resp, next)
-      Cat(toT, Dirty)   -> (true.B,  TtoT, Trunk),
-      Cat(toT, Trunk)   -> (false.B, TtoT, Trunk),
-      Cat(toT, Branch)  -> (false.B, BtoB, Branch),
-      Cat(toT, Nothing) -> (false.B, NtoN, Nothing),
-      Cat(toB, Dirty)   -> (true.B,  TtoB, Branch),
-      Cat(toB, Trunk)   -> (false.B, TtoB, Branch),  // Policy: Don't notify on clean downgrade
-      Cat(toB, Branch)  -> (false.B, BtoB, Branch),
-      Cat(toB, Nothing) -> (false.B, NtoN, Nothing),
-      Cat(toN, Dirty)   -> (true.B,  TtoN, Nothing),
-      Cat(toN, Trunk)   -> (false.B, TtoN, Nothing), // Policy: Don't notify on clean downgrade
-      Cat(toN, Branch)  -> (false.B, BtoN, Nothing), // Policy: Don't notify on clean downgrade
-      Cat(toN, Nothing) -> (false.B, NtoN, Nothing)))
+    val result = ParallelLookUp(
+      Cat(param, state),
+      Seq(
+        // (wanted, am now)  -> (hasDirtyData resp, next)
+        Cat(toT, Dirty)   -> Cat(true.B, TtoT, Trunk),
+        Cat(toT, Trunk)   -> Cat(false.B, TtoT, Trunk),
+        Cat(toT, Branch)  -> Cat(false.B, BtoB, Branch),
+        Cat(toT, Nothing) -> Cat(false.B, NtoN, Nothing),
+        Cat(toB, Dirty)   -> Cat(true.B, TtoB, Branch),
+        Cat(toB, Trunk)   -> Cat(false.B, TtoB, Branch),  // Policy: Don't notify on clean downgrade
+        Cat(toB, Branch)  -> Cat(false.B, BtoB, Branch),
+        Cat(toB, Nothing) -> Cat(false.B, NtoN, Nothing),
+        Cat(toN, Dirty)   -> Cat(true.B, TtoN, Nothing),
+        Cat(toN, Trunk)   -> Cat(false.B, TtoN, Nothing), // Policy: Don't notify on clean downgrade
+        Cat(toN, Branch)  -> Cat(false.B, BtoN, Nothing), // Policy: Don't notify on clean downgrade
+        Cat(toN, Nothing) -> Cat(false.B, NtoN, Nothing),
+      ),
+    )
+    (result(cWidth + width), result(cWidth + width - 1, width), result(width - 1, 0))
   }
 
   /** Translate cache control cmds into Probe param */
   private def cmdToPermCap(cmd: UInt): UInt = {
     import MemoryOpCategories._
     import TLPermissions._
-    MuxLookup(cmd, toN)(Seq(
-      M_FLUSH   -> toN,
-      M_PRODUCE -> toB,
-      M_CLEAN   -> toT))
+    ParallelLookUp(
+      cmd,
+      Seq(
+        M_FLUSH   -> toN,
+        M_PRODUCE -> toB,
+        M_CLEAN   -> toT,
+      ),
+    )
   }
 
   def onCacheControl(cmd: UInt): (Bool, UInt, ClientMetadata) = {
@@ -148,7 +175,7 @@ class ClientMetadata extends Bundle {
     (r._1, r._2, ClientMetadata(r._3))
   }
 
-  def onProbe(param: UInt): (Bool, UInt, ClientMetadata) = { 
+  def onProbe(param: UInt): (Bool, UInt, ClientMetadata) = {
     val r = shrinkHelper(param)
     (r._1, r._2, ClientMetadata(r._3))
   }
--- src/main/scala/util/Misc.scala
+++ src/main/scala/util/Misc.scala
@@ -9,11 +9,13 @@ import chisel3.util.random.LFSR
 import org.chipsalliance.cde.config.Parameters
 import scala.math._
 
-class ParameterizedBundle(implicit p: Parameters) extends Bundle
+class ParameterizedBundle(
+    implicit p: Parameters
+) extends Bundle
 
 trait Clocked extends Bundle {
   val clock = Clock()
-  val reset = Bool()  
+  val reset = Bool()
 }
 
 object DecoupledHelper {
@@ -22,12 +24,14 @@ object DecoupledHelper {
 
 class DecoupledHelper(val rvs: Seq[Bool]) {
   def fire(exclude: Bool, includes: Bool*) = {
-    require(rvs.contains(exclude), "Excluded Bool not present in DecoupledHelper! Note that DecoupledHelper uses referential equality for exclusion! If you don't want to exclude anything, use fire()!")
+    require(
+      rvs.contains(exclude),
+      "Excluded Bool not present in DecoupledHelper! Note that DecoupledHelper uses referential equality for exclusion! If you don't want to exclude anything, use fire()!",
+    )
     (rvs.filter(_ ne exclude) ++ includes).reduce(_ && _)
   }
-  def fire() = {
+  def fire() =
     rvs.reduce(_ && _)
-  }
 }
 
 object MuxT {
@@ -37,7 +41,11 @@ object MuxT {
   def apply[T <: Data, U <: Data, W <: Data](cond: Bool, con: (T, U, W), alt: (T, U, W)): (T, U, W) =
     (Mux(cond, con._1, alt._1), Mux(cond, con._2, alt._2), Mux(cond, con._3, alt._3))
 
-  def apply[T <: Data, U <: Data, W <: Data, X <: Data](cond: Bool, con: (T, U, W, X), alt: (T, U, W, X)): (T, U, W, X) =
+  def apply[T <: Data, U <: Data, W <: Data, X <: Data](
+      cond: Bool,
+      con:  (T, U, W, X),
+      alt:  (T, U, W, X),
+  ): (T, U, W, X) =
     (Mux(cond, con._1, alt._1), Mux(cond, con._2, alt._2), Mux(cond, con._3, alt._3), Mux(cond, con._4, alt._4))
 }
 
@@ -50,7 +58,11 @@ object MuxTLookup {
     res
   }
 
-  def apply[S <: UInt, T <: Data, U <: Data, W <: Data](key: S, default: (T, U, W), mapping: Seq[(S, (T, U, W))]): (T, U, W) = {
+  def apply[S <: UInt, T <: Data, U <: Data, W <: Data](
+      key:     S,
+      default: (T, U, W),
+      mapping: Seq[(S, (T, U, W))],
+  ): (T, U, W) = {
     var res = default
     for ((k, v) <- mapping.reverse)
       res = MuxT(k === key, v, res)
@@ -59,26 +71,23 @@ object MuxTLookup {
 }
 
 object ValidMux {
-  def apply[T <: Data](v1: ValidIO[T], v2: ValidIO[T]*): ValidIO[T] = {
+  def apply[T <: Data](v1: ValidIO[T], v2: ValidIO[T]*): ValidIO[T] =
     apply(v1 +: v2.toSeq)
-  }
   def apply[T <: Data](valids: Seq[ValidIO[T]]): ValidIO[T] = {
     val out = Wire(Valid(valids.head.bits.cloneType))
     out.valid := valids.map(_.valid).reduce(_ || _)
-    out.bits := MuxCase(valids.head.bits,
-      valids.map(v => (v.valid -> v.bits)))
+    out.bits  := MuxCase(valids.head.bits, valids.map(v => v.valid -> v.bits))
     out
   }
 }
 
-object Str
-{
+object Str {
   def apply(s: String): UInt = {
     var i = BigInt(0)
     require(s.forall(validChar _))
     for (c <- s)
       i = (i << 8) | c
-    i.U((s.length*8).W)
+    i.U((s.length * 8).W)
   }
   def apply(x: Char): UInt = {
     require(validChar(x))
@@ -87,12 +96,12 @@ object Str
   def apply(x: UInt): UInt = apply(x, 10)
   def apply(x: UInt, radix: Int): UInt = {
     val rad = radix.U
-    val w = x.getWidth
+    val w   = x.getWidth
     require(w > 0)
 
     var q = x
     var s = digit(q % rad)
-    for (i <- 1 until ceil(log(2)/log(radix)*w).toInt) {
+    for (i <- 1 until ceil(log(2) / log(radix) * w).toInt) {
       q = q / rad
       s = Cat(Mux((radix == 10).B && q === 0.U, Str(' '), digit(q % rad)), s)
     }
@@ -106,16 +115,16 @@ object Str
       Cat(Mux(neg, Str('-'), Str(' ')), Str(abs, radix))
     } else {
       val rad = radix.U
-      val w = abs.getWidth
+      val w   = abs.getWidth
       require(w > 0)
 
-      var q = abs
-      var s = digit(q % rad)
+      var q        = abs
+      var s        = digit(q % rad)
       var needSign = neg
-      for (i <- 1 until ceil(log(2)/log(radix)*w).toInt) {
+      for (i <- 1 until ceil(log(2) / log(radix) * w).toInt) {
         q = q / rad
         val placeSpace = q === 0.U
-        val space = Mux(needSign, Str('-'), Str(' '))
+        val space      = Mux(needSign, Str('-'), Str(' '))
         needSign = needSign && !placeSpace
         s = Cat(Mux(placeSpace, space, digit(q % rad)), s)
       }
@@ -123,37 +132,33 @@ object Str
     }
   }
 
-  private def digit(d: UInt): UInt = Mux(d < 10.U, Str('0')+d, Str(('a'-10).toChar)+d)(7,0)
-  private def validChar(x: Char) = x == (x & 0xFF)
+  private def digit(d:     UInt): UInt = Mux(d < 10.U, Str('0') + d, Str(('a' - 10).toChar) + d)(7, 0)
+  private def validChar(x: Char) = x == (x & 0xff)
 }
 
-object Split
-{
+object Split {
   def apply(x: UInt, n0: Int) = {
     val w = x.getWidth
-    (x.extract(w-1,n0), x.extract(n0-1,0))
+    (x.extract(w - 1, n0), x.extract(n0 - 1, 0))
   }
   def apply(x: UInt, n1: Int, n0: Int) = {
     val w = x.getWidth
-    (x.extract(w-1,n1), x.extract(n1-1,n0), x.extract(n0-1,0))
+    (x.extract(w - 1, n1), x.extract(n1 - 1, n0), x.extract(n0 - 1, 0))
   }
   def apply(x: UInt, n2: Int, n1: Int, n0: Int) = {
     val w = x.getWidth
-    (x.extract(w-1,n2), x.extract(n2-1,n1), x.extract(n1-1,n0), x.extract(n0-1,0))
+    (x.extract(w - 1, n2), x.extract(n2 - 1, n1), x.extract(n1 - 1, n0), x.extract(n0 - 1, 0))
   }
 }
 
-object Random
-{
-  def apply(mod: Int, random: UInt): UInt = {
-    if (isPow2(mod)) random.extract(log2Ceil(mod)-1,0)
-    else PriorityEncoder(partition(apply(1 << log2Up(mod*8), random), mod))
-  }
+object Random {
+  def apply(mod: Int, random: UInt): UInt =
+    if (isPow2(mod)) random.extract(log2Ceil(mod) - 1, 0)
+    else PriorityEncoder(partition(apply(1 << log2Up(mod * 8), random), mod))
   def apply(mod: Int): UInt = apply(mod, randomizer)
-  def oneHot(mod: Int, random: UInt): UInt = {
-    if (isPow2(mod)) UIntToOH(random(log2Up(mod)-1,0))
-    else PriorityEncoderOH(partition(apply(1 << log2Up(mod*8), random), mod)).asUInt
-  }
+  def oneHot(mod: Int, random: UInt): UInt =
+    if (isPow2(mod)) UIntToOH(random(log2Up(mod) - 1, 0))
+    else PriorityEncoderOH(partition(apply(1 << log2Up(mod * 8), random), mod)).asUInt
   def oneHot(mod: Int): UInt = oneHot(mod, randomizer)
 
   private def randomizer = LFSR(16)
@@ -163,7 +168,7 @@ object Random
 
 object Majority {
   def apply(in: Set[Bool]): Bool = {
-    val n = (in.size >> 1) + 1
+    val n       = (in.size >> 1) + 1
     val clauses = in.subsets(n).map(_.reduce(_ && _))
     clauses.reduce(_ || _)
   }
@@ -177,8 +182,8 @@ object PopCountAtLeast {
   private def two(x: UInt): (Bool, Bool) = x.getWidth match {
     case 1 => (x.asBool, false.B)
     case n =>
-      val half = x.getWidth / 2
-      val (leftOne, leftTwo) = two(x(half - 1, 0))
+      val half                 = x.getWidth / 2
+      val (leftOne, leftTwo)   = two(x(half - 1, 0))
       val (rightOne, rightTwo) = two(x(x.getWidth - 1, half))
       (leftOne || rightOne, leftTwo || rightTwo || (leftOne && rightOne))
   }
@@ -196,29 +201,156 @@ object PopCountAtLeast {
 // groupBy applies an interleaved OR reduction; groupBy=2 take 0010 => 01
 object MaskGen {
   def apply(addr_lo: UInt, lgSize: UInt, beatBytes: Int, groupBy: Int = 1): UInt = {
-    require (groupBy >= 1 && beatBytes >= groupBy)
-    require (isPow2(beatBytes) && isPow2(groupBy))
+    require(groupBy >= 1 && beatBytes >= groupBy)
+    require(isPow2(beatBytes) && isPow2(groupBy))
     val lgBytes = log2Ceil(beatBytes)
-    val sizeOH = UIntToOH(lgSize | 0.U(log2Up(beatBytes).W), log2Up(beatBytes)) | (groupBy*2 - 1).U
+    val sizeOH  = UIntToOH(lgSize | 0.U(log2Up(beatBytes).W), log2Up(beatBytes)) | (groupBy * 2 - 1).U
 
-    def helper(i: Int): Seq[(Bool, Bool)] = {
+    def helper(i: Int): Seq[(Bool, Bool)] =
       if (i == 0) {
         Seq((lgSize >= lgBytes.asUInt, true.B))
       } else {
-        val sub = helper(i-1)
+        val sub  = helper(i - 1)
         val size = sizeOH(lgBytes - i)
-        val bit = addr_lo(lgBytes - i)
+        val bit  = addr_lo(lgBytes - i)
         val nbit = !bit
-        Seq.tabulate (1 << i) { j =>
-          val (sub_acc, sub_eq) = sub(j/2)
-          val eq = sub_eq && (if (j % 2 == 1) bit else nbit)
-          val acc = sub_acc || (size && eq)
+        Seq.tabulate(1 << i) { j =>
+          val (sub_acc, sub_eq) = sub(j / 2)
+          val eq                = sub_eq && (if (j % 2 == 1) bit else nbit)
+          val acc               = sub_acc || (size && eq)
           (acc, eq)
         }
       }
-    }
 
-    if (groupBy == beatBytes) 1.U else
-      Cat(helper(lgBytes-log2Ceil(groupBy)).map(_._1).reverse)
+    if (groupBy == beatBytes) 1.U
+    else
+      Cat(helper(lgBytes - log2Ceil(groupBy)).map(_._1).reverse)
   }
+}
+
+object ParallelOperation {
+  def apply[T](xs: Seq[T], func: (T, T) => T): T = {
+    require(xs.nonEmpty)
+    xs match {
+      case Seq(a)    => a
+      case Seq(a, b) => func(a, b)
+      case _ =>
+        apply(Seq(apply(xs take xs.size / 2, func), apply(xs drop xs.size / 2, func)), func)
+    }
+  }
+}
+
+object ParallelOR {
+  def apply[T <: Data](xs: Seq[T]): T =
+    ParallelOperation(xs, (a: T, b: T) => (a.asUInt | b.asUInt).asTypeOf(xs.head))
+}
+
+object ParallelORR {
+  def apply(in: Seq[Bool]): Bool = ParallelOR(in)
+  def apply(in: Bits):      Bool = apply(in.asBools)
+}
+
+object ParallelAND {
+  def apply[T <: Data](xs: Seq[T]): T =
+    ParallelOperation(xs, (a: T, b: T) => (a.asUInt & b.asUInt).asTypeOf(xs.head))
+}
+
+object ParallelANDR {
+  def apply(in: Seq[Bool]): Bool = ParallelAND(in)
+  def apply(in: Bits):      Bool = apply(in.asBools)
+}
+
+object ParallelXOR {
+  def apply[T <: Data](xs: Seq[T]): T =
+    ParallelOperation(xs, (a: T, b: T) => (a.asUInt ^ b.asUInt).asTypeOf(xs.head))
+}
+
+object ParallelMux {
+  def apply[T <: Data](in: Seq[(Bool, T)]): T = {
+    val xs = in map { case (cond, x) => Mux(cond, x, 0.U.asTypeOf(x)) }
+    ParallelOR(xs)
+  }
+  def apply[T <: Data](sel: Seq[Bool], in: Seq[T]): T = apply(sel.zip(in))
+}
+
+object ParallelLookUp {
+  def apply[T <: Data](key: UInt, mapping: Seq[(UInt, T)]): T =
+    ParallelMux(mapping.map(m => (m._1 === key) -> m._2))
+}
+
+object ParallelMax {
+  def apply[T <: Data](xs: Seq[T]): T =
+    ParallelOperation(xs, (a: T, b: T) => Mux(a.asUInt > b.asUInt, a, b).asTypeOf(xs.head))
+}
+
+object ParallelMin {
+  def apply[T <: Data](xs: Seq[T]): T =
+    ParallelOperation(xs, (a: T, b: T) => Mux(a.asUInt < b.asUInt, a, b).asTypeOf(xs.head))
+}
+
+object ParallelPriorityMux {
+  def apply[T <: Data](in: Seq[(Bool, T)]): T =
+    ParallelOperation(in, (a: (Bool, T), b: (Bool, T)) => (a._1 || b._1, Mux(a._1, a._2, b._2)))._2
+  def apply[T <: Data](sel: Bits, in:      Seq[T]): T = apply((0 until in.size).map(sel(_)), in)
+  def apply[T <: Data](sel: Seq[Bool], in: Seq[T]): T = apply(sel zip in)
+}
+
+object ParallelPosteriorityMux {
+  def apply[T <: Data](in: Seq[(Bool, T)]): T =
+    ParallelOperation(in, (a: (Bool, T), b: (Bool, T)) => (a._1 || b._1, Mux(b._1, b._2, a._2)))._2
+  def apply[T <: Data](sel: Bits, in:      Seq[T]): T = apply((0 until in.size).map(sel(_)), in)
+  def apply[T <: Data](sel: Seq[Bool], in: Seq[T]): T = apply(sel zip in)
+}
+
+object ParallelPriorityEncoder {
+  def apply(in: Seq[Bool]): UInt = ParallelPriorityMux(in, (0 until in.size).map(_.asUInt))
+  def apply(in: Bits):      UInt = apply(in.asBools)
+}
+
+object ParallelPosteriorityEncoder {
+  def apply(in: Seq[Bool]): UInt = ParallelPosteriorityMux(in, (0 until in.size).map(_.asUInt))
+  def apply(in: Bits):      UInt = apply(in.asBools)
+}
+
+object ParallelSingedExpandingAdd {
+  def apply(in: Seq[SInt]): SInt = ParallelOperation(in, (a: SInt, b: SInt) => a +& b)
+}
+
+class SelectTwoInterRes[T <: Data](gen: T) extends Bundle {
+  // val valid = Bool()
+  val hasOne = Bool()
+  val hasTwo = Bool()
+  val first  = gen.cloneType
+  val second = gen.cloneType
+}
+
+object SelectTwoInterRes {
+  def apply[T <: Data](hasOne: Bool, hasTwo: Bool, first: T, second: T): SelectTwoInterRes[T] = {
+    val res = Wire(new SelectTwoInterRes(first))
+    res.hasOne := hasOne
+    res.hasTwo := hasTwo
+    res.first  := first
+    res.second := second
+    res
+  }
+  def apply[T <: Data](valid: Bool, data: T): SelectTwoInterRes[T] = {
+    val res = apply(valid, false.B, data, data)
+    res
+  }
+}
+
+object ParallelSelectTwo {
+  def mergeSelectFirstTwo[T <: Data](a: SelectTwoInterRes[T], b: SelectTwoInterRes[T]): SelectTwoInterRes[T] =
+    SelectTwoInterRes(
+      a.hasOne || b.hasOne,
+      a.hasTwo || b.hasTwo || a.hasOne && b.hasOne,
+      Mux(a.hasOne, a.first, b.first),
+      Mux1H(Seq(
+        (!a.hasOne, b.second),
+        (a.hasOne && !a.hasTwo, b.first),
+        (a.hasTwo, a.second),
+      )),
+    )
+  def apply[T <: Data](xs: Seq[SelectTwoInterRes[T]]): SelectTwoInterRes[T] =
+    ParallelOperation(xs, mergeSelectFirstTwo[T])
 }
